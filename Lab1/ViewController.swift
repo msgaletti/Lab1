20ee39db95810558cd8d3868ae3ba0097060ea72
Michelle Galetti <michelle.galetti@gmail.com>
2019-09-08 16:10:57 -0600

    Initial Commit

---
+++ Lab1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Lab1
+//
+//  Created by Michelle Galetti on 9/8/19.
+//  Copyright © 2019 Michelle Galetti. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
