769ec1cbd9c4902f6f92c90c8bfc3ffb0a9c555c
Michelle Galetti <michelle.galetti@gmail.com>
2019-09-10 13:18:41 -0600

    Lab1

--- Lab1/ViewController.swift
+++ Lab1/ViewController.swift
@@ -9,9 +9,33 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var animalImage: UIImageView!
+    
+    
+    
+    @IBOutlet weak var messageText: UILabel!
+    
+    @IBAction func changeImage(_ sender: UIButton) {
+        /*Statistics found on https://www.avma.org/KB/Resources/Statistics/Pages/Market-research-statistics-US-pet-ownership.aspx */
+            if sender.tag == 1{
+                animalImage.image = UIImage(named: "kitten")
+                messageText.text = "30.4% of the US population also love cats!"
+            }
+            else if sender.tag == 2{
+                animalImage.image = UIImage(named: "puppy")
+                messageText.text = "36.5% of the US population also love dogs!"
+            }
+            else if sender.tag == 3{
+                animalImage.image = UIImage(named: "horse")
+                messageText.text = "Horses are expensive animals!"
+            }
+    }
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        animalImage.isUserInteractionEnabled = true
         // Do any additional setup after loading the view.
     }
 
